1c2253fbbb83dec8146bc6a7f98b34d09a8d84f8
Jardel <jardeloliver197@gmail.com>
2025-06-10 21:44:43 -0300

    update compose

--- users_service/src/main/java/com/intelligentclin/users_service/model/dto/CreateUserDto.java
+++ users_service/src/main/java/com/intelligentclin/users_service/model/dto/CreateUserDto.java
@@ -1,9 +1,5 @@
 package com.intelligentclin.users_service.model.dto;
 
-import java.util.Set;
-
-import com.intelligentclin.users_service.model.entity.Role;
-
 public record CreateUserDto(
     String username,
     String password,
