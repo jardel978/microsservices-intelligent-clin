e2301878e3dd9ee57dc065ec75fed8851092971a
Jardel <jardeloliver197@gmail.com>
2025-06-23 14:41:17 -0300

    config and security

--- gateway/src/main/java/com/intelligentclin/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/intelligentclin/gateway/config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
                                 .authorizeExchange(exchange -> exchange
                                                 .pathMatchers(HttpMethod.POST, "/actuator/**").permitAll()
                                                 .pathMatchers(HttpMethod.POST, "/users/create").permitAll()
+                                                .pathMatchers(HttpMethod.POST, "/users/me").permitAll()
                                                 .pathMatchers(HttpMethod.POST, "/users/auth/login").permitAll()
                                                 .pathMatchers(HttpMethod.POST, "/users/auth/refresh").permitAll()
                                                 .anyExchange().authenticated())
