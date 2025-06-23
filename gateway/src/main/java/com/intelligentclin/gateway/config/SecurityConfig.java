7ea09dc62ccda60dea8b068ab85ab62b4d87f94f
Jardel <jardeloliver197@gmail.com>
2025-06-23 14:40:50 -0300

    config and security

--- gateway/src/main/java/com/intelligentclin/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/intelligentclin/gateway/config/SecurityConfig.java
@@ -3,27 +3,45 @@ package com.intelligentclin.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import reactor.core.publisher.Flux;
 
 @Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
+@EnableReactiveMethodSecurity
 public class SecurityConfig {
+
         @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                http
-                                .authorizeHttpRequests(authorize -> authorize
-                                                .requestMatchers(HttpMethod.POST, "/actuator/**").permitAll()
-                                                .anyRequest().authenticated())
-                                .csrf(csrf -> csrf.disable())
-                                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-                                .sessionManagement(session -> session
-                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-                return http.build();
+        public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+                return http
+                                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                                .authorizeExchange(exchange -> exchange
+                                                .pathMatchers(HttpMethod.POST, "/actuator/**").permitAll()
+                                                .pathMatchers(HttpMethod.POST, "/users/create").permitAll()
+                                                .pathMatchers(HttpMethod.POST, "/users/auth/login").permitAll()
+                                                .pathMatchers(HttpMethod.POST, "/users/auth/refresh").permitAll()
+                                                .anyExchange().authenticated())
+                                .oauth2ResourceServer(oauth2 -> oauth2
+                                                .jwt(jwt -> jwt.jwtAuthenticationConverter(
+                                                                grantedAuthoritiesExtractor())))
+                                .build();
+        }
+
+        @Bean
+        public ReactiveJwtAuthenticationConverter grantedAuthoritiesExtractor() {
+                JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+                authoritiesConverter.setAuthorityPrefix("SCOPE_");
+                authoritiesConverter.setAuthoritiesClaimName("scope");
+
+                ReactiveJwtAuthenticationConverter converter = new ReactiveJwtAuthenticationConverter();
+                converter.setJwtGrantedAuthoritiesConverter(jwt ->
+                        Flux.fromIterable(authoritiesConverter.convert(jwt)) // CORREÇÃO AQUI
+                );
+
+                return converter;
         }
 }
--- gateway/src/main/java/com/intelligentclin/gateway/controller/FallBackController.java
+++ gateway/src/main/java/com/intelligentclin/gateway/controller/FallBackController.java
@@ -1,26 +1,27 @@
 package com.intelligentclin.gateway.controller;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Mono;
+
+
 @RestController
 @RequestMapping ("/fallback")
 public class FallBackController {
 
 	@GetMapping ("/clinic")
-	public ResponseEntity<String> moviesFallback () {
-		return new ResponseEntity<>("Servidor de dados médicos indisponível. Por favor, contate o suporte.",
-				HttpStatus.INTERNAL_SERVER_ERROR);
+	public Mono<ResponseEntity<String>> moviesFallback () {
+		return Mono.just(new ResponseEntity<>("Servidor de dados médicos indisponível. Por favor, contate o suporte.",
+				HttpStatus.INTERNAL_SERVER_ERROR));
 	}
 
 	@GetMapping ("/users")
-	public ResponseEntity<String> catalogFallback () {
-		return new ResponseEntity<>("Servidor de usuários indisponível. Por favor, contate o suporte.",
-				HttpStatus.INTERNAL_SERVER_ERROR);
+	public Mono<ResponseEntity<String>> catalogFallback () {
+		return Mono.just(new ResponseEntity<>("Servidor de usuários indisponível. Por favor, contate o suporte.",
+				HttpStatus.INTERNAL_SERVER_ERROR));
 	}
 }
